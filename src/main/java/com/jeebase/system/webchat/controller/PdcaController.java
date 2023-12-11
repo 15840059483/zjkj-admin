e082dbfc184c69aa88295eb83e7628d9d487d34d
root <lu@root.cn>
2023-12-11 16:08:13 +0800

    测试加速器是否成功

--- src/main/java/com/jeebase/system/webchat/controller/PdcaController.java
+++ src/main/java/com/jeebase/system/webchat/controller/PdcaController.java
@@ -22,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
--- src/main/java/com/jeebase/system/webchat/controller/TestController.java
+++ src/main/java/com/jeebase/system/webchat/controller/TestController.java
@@ -7,5 +7,6 @@ package com.jeebase.system.webchat.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("进行更改，和主分支不同");
+        System.out.println("测试加速器是否成功");
     }
 }
