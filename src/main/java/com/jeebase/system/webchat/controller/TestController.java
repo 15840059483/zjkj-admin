d83d12ab65d00b82c4d73f360721f8adb97edaab
root <lu@root.cn>
2023-12-11 16:47:43 +0800

    测试加速器是否成功2.0

--- src/main/java/com/jeebase/system/webchat/controller/TestController.java
+++ src/main/java/com/jeebase/system/webchat/controller/TestController.java
@@ -8,5 +8,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("进行更改，和主分支不同");
         System.out.println("测试加速器是否成功");
+        System.out.println("kljkljkl");
     }
 }
